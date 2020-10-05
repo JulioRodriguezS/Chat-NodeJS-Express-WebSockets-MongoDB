aef7c5cdc2c58e20332b4adfefa184cd5403f21a
JulioRodriguezS <julio.rodriguez.soberano@gmail.com>
2020-10-04 21:06:47 -0500

    uploading app

--- src/public/js/main.js
+++ src/public/js/main.js
@@ -1,1 +1,57 @@
-io()
+$(function(){
+
+    const socket = io()
+
+    //getting dom elements since interface
+    const $messageForm = $('#message-form')
+    const $messageBox = $('#message')
+    const $chat = $('#chat')
+
+    //getting dom elements since interface
+    const $contentWrap = $('#contentWrap') 
+    const $nickWrap = $('#nickWrap')
+    const $nickForm = $('#nickForm')
+    const $nickError = $('#nickError')
+    const $nickName = $('#nickName')
+
+    const $userNames = $('#userNames')
+
+
+    //events
+    $messageForm.submit((e)=>{        
+        e.preventDefault()
+        let message = $messageBox.val()
+        
+        socket.emit('send message',message)
+        $messageBox.val('')
+    })
+
+    $nickForm.submit((e)=>{
+        e.preventDefault()
+        let nickName = $nickName.val()
+
+        socket.emit('new user', nickName,(res)=>{
+            if(res){
+                $nickWrap.hide()
+                $contentWrap.show()
+            }else{
+                $nickError.html(`
+                <div class="alert alert-danger">The nick name is allready exists.</div>
+                `)
+            }
+        })
+        $nickName.val('')
+    })
+
+    socket.on('new message',(data)=>{
+        $chat.append(data,'<br/>')
+    })
+
+    socket.on('user names',(userslist)=>{
+        let list = ''
+        for(user of userslist)
+        list += `<p><i class="fas fa-user"></i> ${user}</p>`
+        $userNames.html(list)
+    })
+
+})
--- src/sockets/sockets.js
+++ src/sockets/sockets.js
@@ -1,5 +1,32 @@
 module.exports = function(io){
+    let nickNames=[]
+
     io.on('connection', newsocket=>{
         console.log('new user connected')
+
+        newsocket.on('send message',(data)=>{
+            io.sockets.emit('new message',data)
+        })
+
+        newsocket.on('new user', (nickname, callback)=>{
+            if(nickNames.indexOf(nickname)!=-1){
+                callback(false)
+            }else{
+                callback(true)
+                newsocket.nickname = nickname
+                nickNames.push(newsocket.nickname)
+                updateNickNames()
+            }
+        })
+        
+        newsocket.on('disconnect',(data)=>{
+            if(!newsocket.nickname)return
+            nickNames.splice(nickNames.indexOf(newsocket.nickname),1)
+            updateNickNames()
+        })
+
+        function updateNickNames(){
+            io.sockets.emit('user names',nickNames)
+        }
     })
 }
